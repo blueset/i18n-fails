13a4ee3d46c48d89a9ecf429a5f9ad3b365f7870
Eana Hufwe <eana@1a23.com>
2025-01-25 19:45:18 -0800

    ci: fix build pipeline

--- src/components/AdminBar/index.tsx
+++ src/components/AdminBar/index.tsx
@@ -5,7 +5,7 @@ import type { PayloadAdminBarProps, PayloadMeUser } from 'payload-admin-bar'
 import { cn } from '@/utilities/ui'
 import { useSelectedLayoutSegments } from 'next/navigation'
 import { PayloadAdminBar } from 'payload-admin-bar'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 import './index.scss'
@@ -27,6 +27,24 @@ const collectionLabels = {
     plural: 'Projects',
     singular: 'Project',
   },
+  languages: {
+    plural: 'Languages',
+    singular: 'Language',
+  },
+  categories: {
+    plural: 'Categories',
+    singular: 'Category',
+  },
+  products: {
+    plural: 'Products',
+    singular: 'Product',
+  },
+}
+
+const segmentCollectionMapping = {
+  vendors: 'products',
+  'source-languages': 'languages',
+  'destination-languages': 'languages',
 }
 
 const Title: React.FC = () => <span>Dashboard</span>
@@ -37,14 +55,39 @@ export const AdminBar: React.FC<{
   const { adminBarProps } = props || {}
   const segments = useSelectedLayoutSegments()
   const [show, setShow] = useState(false)
+  const mappedSegment = (segmentCollectionMapping[
+    (segments?.[0] ?? '') as keyof typeof segmentCollectionMapping
+  ] ?? segments?.[0]) as keyof typeof collectionLabels
   const collection = (
-    collectionLabels[segments?.[1] as keyof typeof collectionLabels] ? segments[1] : 'pages'
+    collectionLabels[mappedSegment] ? mappedSegment : 'pages'
   ) as keyof typeof collectionLabels
   const router = useRouter()
 
   const onAuthChange = React.useCallback((user: PayloadMeUser) => {
     setShow(Boolean(user?.id))
   }, [])
+
+  const [collectionId, setCollectionId] = useState<string | undefined>(undefined)
+  useEffect(() => {
+    if (!show) return
+    const slug = segments?.[1]
+    if (collection && slug) {
+      fetch(`/api/${collection}?where[slug][equals]=${slug}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data?.docs?.[0]?.id) {
+            setCollectionId(data?.docs?.[0]?.id)
+          } else {
+            setCollectionId(undefined)
+          }
+        })
+        .catch(() => {
+          setCollectionId(undefined)
+        })
+    } else {
+      setCollectionId(undefined)
+    }
+  }, [collection, segments])
 
   return (
     <div
@@ -68,6 +111,7 @@ export const AdminBar: React.FC<{
             plural: collectionLabels[collection]?.plural || 'Pages',
             singular: collectionLabels[collection]?.singular || 'Page',
           }}
+          id={collectionId}
           logo={<Title />}
           onAuthChange={onAuthChange}
           onPreviewExit={() => {
