f65dee4631e813f5e770ebcac57f61c6d6594006
Eana Hufwe <eana@1a23.com>
2025-01-26 10:00:28 -0800

    fix: home seed

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -50,16 +50,16 @@ export default async function Page({ params: paramsPromise }: Args) {
   const { slug = 'home' } = await paramsPromise
   const url = '/' + slug
 
-  let page: PageType | null
+  // let page: PageType | null
 
-  page = await queryPageBySlug({
+  const page = await queryPageBySlug({
     slug,
   })
 
   // Remove this code once your website is seeded
-  if (!page && slug === 'home') {
-    page = homeStatic
-  }
+  // if (!page && slug === 'home') {
+  //   page = homeStatic
+  // }
 
   if (!page) {
     return <PayloadRedirects url={url} />
@@ -107,6 +107,8 @@ const queryPageBySlug = cache(async ({ slug }: { slug: string }) => {
       },
     },
   })
+
+  console.log('queryPageBySlug', slug, 'result', result)
 
   return result.docs?.[0] || null
 })
